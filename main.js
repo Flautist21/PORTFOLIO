d06a46800153b78522fb77f5c1d1dd8e036bde7b
Flautist21 <harshjaiswal2101@gmail.com>
2024-04-23 14:59:54 +0530

    It's my commit for portfolio project

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Flautist", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+  });
+  
